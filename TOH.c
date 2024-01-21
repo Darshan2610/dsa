728f9beb4314d7fa50c5235f38c9c159a0746e71
Darshan Jain <116780686+Darshan2610@users.noreply.github.com>
2024-01-21 21:33:23 +0530

    Create TOH.c

---
+++ TOH.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+//Global variable declaration
+int count = 0;
+//Tower of Hanoi function definition
+
+void TOH(int n,char A,char B,char C)
+
+{
+
+	if(n>0)
+
+	{
+
+		TOH(n-1, A, C, B);
+
+		printf("Disk %d moved from %c --> %c:\n", n, A, C);
+
+		count++;
+
+		TOH(n-1,B,A,C);
+
+	}
+
+}
+
+//main function
+
+void main()
+
+{
+
+	int n;
+
+	printf("Enter the number of disks:");
+
+	scanf("%d",&n);
+
+	TOH(n,'A','B','C');
+
+	printf("Number of moves taken to move disks from source to destination:  %d",count);
+
+}
